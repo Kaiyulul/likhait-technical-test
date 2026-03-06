2e194c77ddb7ee36abf46753d4853217d1399add
Kyle Tandoc <kyletandoc@gmail.com>
2026-03-06 17:43:23 +0800

    BE: Added category creation API, added validations, updated migrations, and updated tests

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :expenses, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
+  validates :icon, length: { maximum: 16 }, allow_blank: true
 end
